aacde74900d6218834bd2caf571a37682c32a2a6
ye909 <yesidprogramacion711@gmail.com>
2025-04-16 20:23:44 -0500

    poniendo imagenes a la opcion de ventas

---
+++ .history/src/js/ejercicios_20250415223205.js
@@ -0,0 +1,51 @@
+
+alert("¡Los JJOO de París 2024 han comenzado!")
+
+while (true) {
+   const participante1 = prompt("participar 1")
+   const registro1 = prompt("registro evento 1")
+   const simulacion = prompt("simulacion eventos")
+   const Creacióninformes = prompt("registro evento 2")
+   const Salir = prompt("participar 3")
+   const opcion= prompt("selecione una de las opciones")
+
+     switch (opcion) {
+        case participante1:
+            console.log(1)
+            break;
+            case registro1:
+                console.log(1)
+                break;
+                case simulacion:
+                    console.log(1)
+                    break;
+                    case Creacióninformes:
+                        break
+                    case Salir:
+                    break
+        default:
+            console.log("no")
+            break;
+     }
+}
+
+// * EJERCICIO:
+//  * ¡Los JJOO de París 2024 han comenzado!
+//  * Crea un programa que simule la celebración de los juegos.
+//  * El programa debe permitir al usuario registrar eventos y participantes,
+//  * realizar la simulación de los eventos asignando posiciones de manera aleatoria
+//  * y generar un informe final. Todo ello por terminal.
+//  * Requisitos:
+//  * 1. Registrar eventos deportivos.
+//  * 2. Registrar participantes por nombre y país.
+//  * 3. Simular eventos de manera aleatoria en base a los participantes (mínimo 3).
+//  * 4. Asignar medallas (oro, plata y bronce) basándose en el resultado del evento.
+//  * 5. Mostrar los ganadores por cada evento.
+//  * 6. Mostrar el ranking de países según el número de medallas.
+//  * Acciones:
+//  * 1. Registro de eventos.
+//  * 2. Registro de participantes.
+//  * 3. Simulación de eventos.
+//  * 4. Creación de informes.
+//  * 5. Salir del programa.
+//  */
---
+++ .history/src/js/ejercicios_20250415224130.js
@@ -0,0 +1,53 @@
+
+alert("¡Los JJOO de París 2024 han comenzado!")
+
+// while (true) {
+//    const participante1 = prompt("participar 1")
+//    const registro1 = prompt("registro evento 2 ")
+//    const simulacion = prompt("simulacion eventos 3")
+//    const Creacióninformes = prompt("Creación de informes 4")
+//    const Salir = prompt("salir 5")
+   const opcion= prompt("selecione una de las opciones")
+
+     switch (opcion) {
+        case "1":
+            console.log("uno")
+            break;
+            case "2":
+                console.log("dos")
+                break;
+                case "3":
+                    console.log("tres")
+                    break;
+                    case "4":
+                        console.log("cuatro")
+                        break
+                    case "5":
+                        aler("cinco")
+                    break
+        default:
+            console.log("no")
+            break;
+     }
+// }
+
+// * EJERCICIO:
+//  * ¡Los JJOO de París 2024 han comenzado!
+//  * Crea un programa que simule la celebración de los juegos.
+//  * El programa debe permitir al usuario registrar eventos y participantes,
+//  * realizar la simulación de los eventos asignando posiciones de manera aleatoria
+//  * y generar un informe final. Todo ello por terminal.
+//  * Requisitos:
+//  * 1. Registrar eventos deportivos.
+//  * 2. Registrar participantes por nombre y país.
+//  * 3. Simular eventos de manera aleatoria en base a los participantes (mínimo 3).
+//  * 4. Asignar medallas (oro, plata y bronce) basándose en el resultado del evento.
+//  * 5. Mostrar los ganadores por cada evento.
+//  * 6. Mostrar el ranking de países según el número de medallas.
+//  * Acciones:
+//  * 1. Registro de eventos.
+//  * 2. Registro de participantes.
+//  * 3. Simulación de eventos.
+//  * 4. Creación de informes.
+//  * 5. Salir del programa.
+//  */
